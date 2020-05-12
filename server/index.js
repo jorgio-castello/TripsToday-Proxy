f66059a9e4ebe6a502cfd5ce8f4da417895d7868
Jorge Castello <jorgio.castello@gmail.com>
2020-05-12 10:10:16 -0700

    Create proxy server index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('client'));
+
 
 app.listen(2400, () => console.log('Proxy Server Listening to Requests on Port 2400...'));
