7463a07a88ec7e6b3b2fad6835b63775d34bbb92
Jorge Castello <jorgio.castello@gmail.com>
2020-05-12 16:26:09 -0700

    Install Cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.static('client'));
+app.use(cors());
+
 
 
 app.listen(2400, () => console.log('Proxy Server Listening to Requests on Port 2400...'));
