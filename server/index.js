3fdc7139b73621e8d2509710ef116760aa30ad81
Jorge Castello <jorgio.castello@gmail.com>
2020-05-12 20:45:00 -0700

    Implement proxy routing for bookings component

--- server/index.js
+++ server/index.js
@@ -13,4 +13,21 @@ app.get('/trip/gallery/:id', (req, res) => {
   .catch(err => console.log(err));
 });
 
+app.get('/trip/bookings/data/:id', (req, res) => {
+  const { id } = req.params;
+  fetch(`http://127.0.0.1:3001/api/trip/${id}/price`)
+  .then(response => response.json())
+  .then(data => res.send(data))
+  .catch(err => console.log(err));
+});
+
+app.get('/trip/bookings/price/:id/:date/:adults', (req, res) => {
+  const { id, date, adults } = req.params;
+  fetch(`http://127.0.0.1:3001/api/trip/${id}/calendar/?startdate=${date}&enddate=${date}&adults=${adults}`)
+  .then(response => response.json())
+  .then(data => res.send(data))
+  .catch(err => console.log(err));
+});
+
+
 app.listen(2400, () => console.log('Proxy Server Listening to Requests on Port 2400...'));
