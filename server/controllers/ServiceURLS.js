421aba0d0aa85746c135e6f01516b211e2937146
Jorge Castello <jorgio.castello@gmail.com>
2020-06-16 13:34:55 -0700

    Refactor URLS / ports into their own master file

---
+++ server/controllers/ServiceURLS.js
@@ -0,0 +1,11 @@
+module.exports.galleryURL = process.env.GALLERY_URL || 'http://localhost';
+module.exports.galleryPort = process.env.GALLERY_PORT || 9999;
+
+module.exports.bookingURL = process.env.BOOKING_URL || 'http://localhost';
+module.exports.bookingPort = process.env.BOOKING_PORT || 3001;
+
+module.exports.itineraryURL = process.env.ITINERARY_URL || 'http://localhost';
+module.exports.itineraryPort = process.env.ITINERARY_PORT || 3000;
+
+module.exports.reviewsURL = process.env.REVIEWS_URL || 'http://localhost';
+module.exports.reviewsPort = process.env.REVIEWS_PORT || 4000;
