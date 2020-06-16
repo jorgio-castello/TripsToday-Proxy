951b8797484d114c799341c145c1a04469aed914
Jorge Castello <jorgio.castello@gmail.com>
2020-06-16 12:50:27 -0700

    Refactor server routes into controllers file

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require('express');
 const compression = require('compression');
 const cache = require('express-cache-controller');
@@ -5,44 +6,25 @@ const app = express();
 const cors = require('cors');
 const fetch = require('node-fetch');
 
-// app.use(cache({maxAge: 31536000}));
-// app.use(express.json());
-// app.use(compression());
+// Express configuration
+app.use(cache({maxAge: 31536000}));
+app.use(express.json());
+app.use(compression());
 app.use(express.static('client'));
 app.use(express.json());
 app.use(cors());
 
-app.get('/tripAdvisor/:id/gallery', (req, res) => {
-  fetch(`http://54.215.51.250/tripAdvisor/${req.params.id}/gallery`)
-  .then(response => response.json())
-  .then(data => res.send(data))
-  .catch(err => console.log(err));
-});
+// Controllers
+const { getGallery, getPrice, getCalendar, getTour } = require('./controllers');
 
-app.get('/api/trip/:id/price', (req, res) => {
-  const { id } = req.params;
-  fetch(`http://3.12.241.47/api/trip/${id}/price`)
-  .then(response => response.json())
-  .then(data => res.send(data))
-  .catch(err => console.log(err));
-});
+app.get('/tripAdvisor/:id/gallery', getGallery);
 
-app.get('/api/trip/:id/calendar/?', (req, res) => {
-  const { id } = req.params;
-  const { startdate, adults } = req.query;
 
-  fetch(`http://3.12.241.47/api/trip/${id}/calendar/?startdate=${startdate}&enddate=${startdate}&adults=${adults}`)
-  .then(response => response.json())
-  .then(data => res.send(data))
-  .catch(err => console.log(err));
-});
+app.get('/api/trip/:id/price', getPrice);
 
-app.get('/tour', (req, res) => {
-  fetch('http://34.222.61.220/tour')
-  .then(response => response.json())
-  .then(data => res.send(data))
-  .catch(err => console.log(err));
-});
+app.get('/api/trip/:id/calendar/?', getCalendar);
+
+app.get('/tour', getTour);
 
 app.get('/reviews', (req, res) => {
   fetch('http://18.217.110.214:3004/reviews')
