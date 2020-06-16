7ee6b35496f8925d2d539d8e77b2287145e246bc
Jorge Castello <jorgio.castello@gmail.com>
2020-06-16 13:03:36 -0700

    Add Reviews configuration / update PORT

--- server/index.js
+++ server/index.js
@@ -15,32 +15,17 @@ app.use(express.json());
 app.use(cors());
 
 // Controllers
-const { getGallery, getPrice, getCalendar, getTour } = require('./controllers');
+const { getGallery, getPrice, getCalendar, getTour, getReviews, updateReview } = require('./controllers');
 
+// Routes
 app.get('/tripAdvisor/:id/gallery', getGallery);
-
-
 app.get('/api/trip/:id/price', getPrice);
-
 app.get('/api/trip/:id/calendar/?', getCalendar);
-
 app.get('/tour', getTour);
+app.get('/reviews', getReviews);
+app.put('/reviews', updateReview);
 
-app.get('/reviews', (req, res) => {
-  fetch('http://18.217.110.214:3004/reviews')
-  .then(response => response.json())
-  .then(data => res.send(data))
-  .catch(err => console.log(err));
-});
+// Port Configuration
+const PORT = process.env.port || 2400;
 
-app.put('/reviews', ({body: { _id }}, res) => { // nested destructuring
-  fetch('http://18.217.110.214:3004/reviews', {
-    method: 'PUT',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({ _id })
-  })
-  .then(res => res.json())
-  .then(data => res.send(data));
-});
-
-app.listen(2400, () => console.log('Proxy Server Listening to Requests on Port 2400...'));
+app.listen(PORT, () => console.log(`Proxy Server Listening to Requests on Port ${PORT}...`));
